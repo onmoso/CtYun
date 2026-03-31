b62918becaab16aec01e9f19c453b21a6462e1b9
onmoso <64882073+onmoso@users.noreply.github.com>
2026-03-31 12:57:23 +0800

    Update Program.cs

--- CtYun/Program.cs
+++ CtYun/Program.cs
@@ -36,9 +36,9 @@ foreach (var d in desktopList)
     {
         Utility.WriteLine(ConsoleColor.Red, $"[{d.DesktopCode}] [{d.UseStatusText}]电脑未开机，正在开机，请在2分钟后重新运行软件");
     }
-    Utility.WriteLine(ConsoleColor.Red, d.UseStatusText);
+    Utility.WriteLine(ConsoleColor.Red, $"[{d.DesktopCode}] [{d.UseStatusText}]");
     var connectResult = await cyApi.ConnectAsync(d.DesktopId);
-    Utility.WriteLine(ConsoleColor.Red, connectResult);
+    Utility.WriteLine(ConsoleColor.Red, "ssssssssssssssss");
     if (connectResult.Success && connectResult.Data.DesktopInfo != null)
     {
         d.DesktopInfo = connectResult.Data.DesktopInfo;
